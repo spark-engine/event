8d8b71f95894c85362e5c61d037cfe4382740fb2
Brandon Mathis <brandon@imathis.com>
2016-03-21 13:29:23 -0500

    Working tests

--- test/events.test.js
+++ test/events.test.js
@@ -1,12 +1,45 @@
 var assert = require('chai').assert
 var event = require('../')
 
-describe('Event', function(){
+document.querySelector('body').innerHTML = '<div id="test"></div>'
+var testEl = document.querySelector('#test')
+
+describe('DomReady', function(){
   before(function(){
-    event.load(function(){ document.write('hi') })
+    event.ready(function(){ testEl.textContent = "Document Ready Fired" })
   })
-  it('Load function works', function(){
-    assert.equal(document.textContent, 'hi')
+  it('Document ready function works', function(){
+    event.fire(document, 'DOMContentLoaded')
+    assert.equal(testEl.textContent, 'Document Ready Fired')
+
+  })
+})
+
+describe('Test regular event', function(){
+  before(function(){
+    event.on(document, 'dang', '#test', function() {
+      testEl.textContent = 'regular event'
+    })
+  })
+
+  it('Test regular event', function(){
+    event.fire(testEl, 'dang')
+    assert.equal(testEl.textContent, 'regular event')
+  })
+})
+
+// PhantomJS doesn't do animation events, but this should pass because
+// no support for animation events means that it will fire them immediately
+describe('Test animation event failure', function(){
+  before(function(){
+    event.on(document, 'animationend', '#test', function() {
+      testEl.textContent = 'animation end'
+    })
+  })
+
+  it('Test regular event', function(){
+    event.fire(testEl, 'animationend')
+    assert.equal(testEl.textContent, 'animation end')
   })
 })
 
