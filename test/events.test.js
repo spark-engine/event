bfa82686b92d89071345d7176e94cb0ed976956d
Brandon Mathis <brandon@imathis.com>
2016-03-21 16:28:27 -0500

    Added failing test for turning off events

--- test/events.test.js
+++ test/events.test.js
@@ -11,26 +11,47 @@ describe('DomReady', function(){
   it('Document ready function works', function(){
     event.fire(document, 'DOMContentLoaded')
     assert.equal(testEl.textContent, 'Document Ready Fired')
-
   })
 })
 
 describe('Test regular event', function(){
   before(function(){
-    event.on(document, 'dang', '#test', function() {
+    event.on(document, 'test', '#test', function() {
       testEl.textContent = 'regular event'
     })
   })
 
   it('Test regular event', function(){
-    event.fire(testEl, 'dang')
+    event.fire(testEl, 'test')
     assert.equal(testEl.textContent, 'regular event')
+  })
+})
+
+describe('Test event removal', function(){
+  before(function(){
+    var called = false
+
+    event.on(document, 'test', '#test', function() {
+      if(!called) {
+        testEl.textContent = 'called event'
+      } else {
+        testEl.textContent = 'called event again'
+      }
+      called = true
+    })
+  })
+
+  it('Test regular event', function(){
+    event.fire(testEl, 'test')
+    event.off(testEl,  'test')
+    event.fire(testEl, 'test')
+    assert.equal(testEl.textContent, 'called event')
   })
 })
 
 // PhantomJS doesn't do animation events, but this should pass because
 // no support for animation events means that it will fire them immediately
-describe('Test animation event failure', function(){
+describe('Test animation event', function(){
   before(function(){
     event.on(document, 'animationend', '#test', function() {
       testEl.textContent = 'animation end'
