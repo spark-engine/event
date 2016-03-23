91b3a4ff90f3c0bb256f419229c4f8defa155613
Brandon Mathis <brandon@imathis.com>
2016-03-23 16:45:58 -0500

    Refactored transform between listener management and bean, added tap event

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var bean = require('bean')
 var key = require('keymaster')
 var animationEvent = require('./lib/animation-events.js')
 var page = require('./lib/page-events.js')
+var tap = require('./lib/tap-events.js')
 var slice = Array.prototype.slice
 
 module.exports = {
@@ -18,29 +19,35 @@ module.exports = {
   oneKey: oneKey
 }
 
+// Bean doesn't account for cross-browser support on animation events
+// So rather than pass through events to bean, we process them to add
+// vendor prefixes or remove events if browsers do not suppor them
+//
 function on () {
-  var args = transformArgs(slice.call(arguments))
-  if(args) {
-    bean.on.apply(null, args)
-  }
+  setEvent('on', slice.call(arguments))
 }
 
 function off () {
-  var args = transformArgs(slice.call(arguments))
-
-  if(args) {
-    bean.off.apply(null, args)
-  }
+  setEvent('off', slice.call(arguments))
 }
 
 function one () {
-  var args = transformArgs(slice.call(arguments))
+  setEvent('one', slice.call(arguments))
+}
 
-  if(args) {
-    bean.one.apply(null, args)
+function setEvent(type, args) {
+  // Process animation events for browser-support
+  args = transformArgs(args)
+
+  // If no events remain (Because of no browser support)
+  // do not register events
+  if( !isEmpty(args[1]) ) {
+    bean[type].apply(null, args)
   }
 }
 
+// Add support for unbinding a key event after it is called
+//
 function oneKey (keys, scope, fn) {
   if (typeof scope == 'function') {
     fn = scope
@@ -53,7 +60,8 @@ function oneKey (keys, scope, fn) {
   })
 }
 
-// Ensure
+// Transform event arguments to handle tap event and cross-browser animation events
+//
 function transformArgs(args) {
   var newEvents = {}
   var events = args[1]
@@ -65,57 +73,47 @@ function transformArgs(args) {
   if (typeof events == 'object') {
     for (type in events) {
       if (events.hasOwnProperty(type)) {
+        var callback = events[type]
 
         // Adds vendor prefixes or calls function if browser doesn't support animation events
-        transformed = transformAnimationEvents(type, events[type])
+        //
+        var transformed = animationEvent.transform(type, callback)
 
-        if (transformed.length > 0) {
-          newEvents[transformed] = events[type]
+        // Walk through each event and if it should be a tap event
+        // replace the callback with a tap wrapped callback
+        //
+        if (transformed.match(/tap/)) {
+          var remainingEvents = []
+
+          transformed.split(' ').forEach(function(e){
+            if (e.match(/tap/, '')) {
+              // Direcly add the event as a tap event
+              newEvents.touchstart = tap(callback)
+            } else { 
+              remainingEvents.push(e)
+            }
+          })
+          transformed = remainingEvents.join(' ')
+        }
+
+        if (0 < transformed.length) {
+          newEvents[transformed] = callback
         }
       }
     }
+
     args[1] = newEvents
+
   } else {
-    args[1] = transformAnimationEvents(events, args[3] || args[2])
+
+    // If events are just a string, replace animation events with with browser supported ones
+    //
+    args[1] = animationEvent.transform(events, args[3] || args[2])
   }
 
-  if (!isEmpty(args[1])) return args
-
+  return args
 }
 
-// Browser support: As necessary add vendor prefixes or camelCased event names
-//
-function transformAnimationEvents (events, fn) {
-  eventTypes = []
-  
-  events.split(' ').forEach(function(e){
-    if (e.match(/animation/i)) {
-
-      if(!animationEvent.supported) { 
-
-        if(window.env != 'test') {
-          console.error('Animation events are not supported')
-        }
-
-        // If animation events aren't supported trigger immediately
-        fn()
-
-      } else if (animationEvent.types[e]) {
-        
-        // Fetch properly prefixed, cased event names from Animation Events lib
-        eventTypes.push(animationEvent.types[e])
-
-      } else {
-        console.error('"' + e + '" is not a supported animation event')
-      }
-    } else {
-      eventTypes.push(e)
-    }
-  })
-
-  return eventTypes.join(' ')
-  
-}
 
 function isEmpty(obj) {
   var hasOwnProperty = Object.prototype.hasOwnProperty
